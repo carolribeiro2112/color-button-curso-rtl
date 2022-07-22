1d54ef10466811ea7f9520c7a26eb8322f5b89e0
Ana Carolina Ribeiro <carolribeiro2112@gmail.com>
2022-07-22 09:01:37 -0300

    feat: test for the button

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('the button has the correct initial color', () => {
+  render(<App/>);
+  //find an element with role of button and text of 'Change to blue'
+  const buttonElement = screen.getByRole('button', {name: 'Change to blue'});
+  // expect the background color to be red
+  expect(buttonElement).toHaveStyle({backgroundColor:'red'});
+  // click the button
+  fireEvent.click(buttonElement);
+  // expect the background color to be blue
+  expect(buttonElement).toHaveStyle({backgroundColor:'blue'});
+  // expect the button text to be 'Change to red'
+  expect(buttonElement.textContent).toBe('Change to red');
 });
